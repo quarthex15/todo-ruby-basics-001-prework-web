0206ad8758cc2e1faf451efd15db3a367c3a08ea
quarthex15 <andrew.klein.j@gmail.com>
2016-04-17 20:07:33 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  "I'm right and you are wrong!"
 end
 
-def greeting
+def greeting(name,place)
 
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
